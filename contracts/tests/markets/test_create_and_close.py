b45c5c4ce560e94a2e3975274e9346fabb720135
Alexey Akhunov <alexeyakhunov@Alexeys-MBP.home>
2017-06-29 22:13:11 +0100

    Gas measurements for market creation and closing

--- contracts/tests/markets/test_create_and_close.py
+++ contracts/tests/markets/test_create_and_close.py
@@ -43,3 +43,43 @@ class TestContracts(AbstractTestContracts):
         # Sell all outcomes
         event.sellAllOutcomes(funding, sender=keys[buyer])
         self.assertEqual(self.ether_token.balanceOf(accounts[buyer]), funding * 2)
+
+    def test_gas_cost_creation(self):
+        # Create event
+        ipfs_hash = b'QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG'
+        oracle = self.centralized_oracle_factory.createCentralizedOracle(ipfs_hash)
+        gas_costs = []
+        for num_outcomes in xrange(2, 14):
+            event = self.contract_at(self.event_factory.createCategoricalEvent(self.ether_token.address, oracle, num_outcomes), self.event_abi)
+            # Create market
+            fee = 0
+            with self.gas_counter() as gc:
+                self.market_factory.createMarket(event.address, self.lmsr.address, fee)
+            gas_costs.append(gc.gas_cost())
+        self.assertEqual([1690607L, 1695677L, 1700747L, 1705817L, 1710887L, 1715893L, 1721027L, 1726097L, 1731167L,
+                          1736237L, 1741243L, 1746377L], gas_costs)
+
+    def test_gas_cost_close(self):
+        # Create event
+        ipfs_hash = b'QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG'
+        oracle = self.centralized_oracle_factory.createCentralizedOracle(ipfs_hash)
+        gas_costs = []
+        for num_outcomes in xrange(2, 14):
+            event = self.contract_at(self.event_factory.createCategoricalEvent(self.ether_token.address, oracle, num_outcomes), self.event_abi)
+            # Create market
+            fee = 0
+            market = self.contract_at(self.market_factory.createMarket(event.address, self.lmsr.address, fee), self.market_abi)
+            # Fund market
+            buyer = 0
+            funding = 100
+            self.ether_token.deposit(value=funding, sender=keys[buyer])
+            self.assertEqual(self.ether_token.balanceOf(accounts[buyer]), funding)
+            self.ether_token.approve(market.address, funding, sender=keys[buyer])
+            market.fund(funding, sender=keys[buyer])
+            # Close market
+            with self.gas_counter() as gc:
+                market.close(sender=keys[buyer])
+            gas_costs.append(gc.gas_cost())
+        self.assertEqual([63604L, 81222L, 98840L, 116458L, 134076L, 151694L, 169312L, 186930L, 204548L,
+                          222166L, 239784L, 257402L], gas_costs)
+
