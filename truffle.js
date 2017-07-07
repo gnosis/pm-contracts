b0f24e68f22189a029116855482e026964b8533e
Alan Lu <alanlu1023@gmail.com>
2017-07-07 08:30:36 -0500

    Remove HD wallet provider dep for Travis

--- truffle.js
+++ truffle.js
@@ -1,14 +1,3 @@
-const HDWalletProvider = require("truffle-hdwallet-provider")
-
-//address info for acct used to deploy contracts on kovan
-//address: 0x00200eB02C014831EBa45978415c30cf7110b329
-//*note: parity wallet was used to deploy (not remote provider)
-
-//address info for acct used to deploy contracts on rinkeby
-//address: 0x069fd4784D1DEd8A63923e83fF73c44414240043
-let mnemonic = 'stool arrow fatigue sunny actual bind radio license enemy peanut penalty soccer';
-
-
 module.exports = {
     networks: {
         development: {
@@ -22,10 +11,9 @@ module.exports = {
             network_id: "42"
         },
         rinkeby: {
-            provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io"),
-            network_id: "4",
-            gas: 4500000,
-            gasPrice: 25000000000
+            host: "localhost",
+            port: 8545,
+            network_id: "4"
         },
     }
 }
