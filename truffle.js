8edd2ccb189733f25cd7bbbeff45446a4b561584
Alan Lu <alanlu1023@gmail.com>
2017-07-11 14:31:19 -0500

    Allow Travis to run coverage

--- truffle.js
+++ truffle.js
@@ -5,6 +5,15 @@ module.exports = {
             port: 8545,
             network_id: "*" // Match any network id
         },
+        coverage: {
+            host: "localhost",
+            network_id: "*",
+            port: 8555,
+            gas: 0xfffffffffff,
+            gasPrice: 0x01,
+            before_timeout: 0,
+            test_timeout: 0
+        },
         kovan: {
             host: "localhost",
             port: 8545,
