362e41f641405fdd3f03fb77e48c42e6f7cd7fb9
Alan Lu <alanlu1023@gmail.com>
2018-01-24 11:40:23 +0100

    Unpin solc to ^0.4.15

--- scripts/makedocs.sh
+++ scripts/makedocs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 npm i @digix/doxity
-find contracts test -name '*.sol' -type f -print0 | xargs -0 sed -i -e 's/pragma solidity 0/pragma solidity ^0/'
 node_modules/.bin/doxity init
 sed -i -e 's/linkPrefix = .*/linkPrefix = "\/gnosis-contracts"/' scripts/doxity/config.toml
 node_modules/.bin/doxity build
