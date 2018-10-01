eae425ac9ea739655349fe44c32958d8af85db97
Alan Lu <alanlu1023@gmail.com>
2018-10-01 17:10:24 -0500

    Remove outcome tokens; use internal bookkeeping instead

--- contracts/ConditionalPaymentProcessor.sol
+++ contracts/ConditionalPaymentProcessor.sol
@@ -1,18 +1,15 @@
 pragma solidity ^0.4.24;
-import "openzeppelin-solidity/contracts/token/ERC20/StandardBurnableToken.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./OracleConsumer.sol";
-
-contract OutcomeToken is MintableToken, StandardBurnableToken {}
 
 contract ConditionalPaymentProcessor is OracleConsumer {
     using SafeMath for uint;
 
-    event ConditionPreparation(bytes32 indexed conditionId, address indexed oracle, bytes32 indexed questionId, uint outcomeTokenCount);
-    event ConditionResolution(bytes32 indexed conditionId, address indexed oracle, bytes32 indexed questionId, uint outcomeTokenCount, bytes result);
-    event OutcomeTokenSetMinting(bytes32 indexed conditionId, uint amount);
-    event OutcomeTokenSetBurning(bytes32 indexed conditionId, uint amount);
+    event ConditionPreparation(bytes32 indexed conditionId, address indexed oracle, bytes32 indexed questionId, uint payoutSlotCount);
+    event ConditionResolution(bytes32 indexed conditionId, address indexed oracle, bytes32 indexed questionId, uint payoutSlotCount, bytes result);
+    event PositionSplit(address indexed stakeholder, bytes32 indexed splitSlotId, bytes32 indexed conditionId, uint amount);
+    event PositionMerge(address indexed stakeholder, bytes32 indexed mergedSlotId, bytes32 indexed conditionId, uint amount);
     event PayoutRedemption(address indexed redeemer, uint payout);
 
     ERC20 public collateralToken;
@@ -21,39 +18,30 @@ contract ConditionalPaymentProcessor is OracleConsumer {
     mapping(bytes32 => uint[]) public payoutNumerators;
     mapping(bytes32 => uint) public payoutDenominator;
 
-    /// Mapping key is an outcomeTokenId, composed of (conditionId, index) pair
-    mapping(bytes32 => OutcomeToken) internal outcomeTokens;
+    /// First key is the address of an account holder who has a stake in some payout slot for a condition.
+    /// Second key is an payoutSlotId, composed of (conditionId, index) pair.
+    /// The result of the mapping is the amount of stake held in a corresponding payout slot by the account holder.
+    mapping(address => mapping(bytes32 => uint)) internal positions;
 
     constructor(ERC20 _collateralToken) public {
         collateralToken = _collateralToken;
     }
 
-    function prepareCondition(address oracle, bytes32 questionId, uint outcomeTokenCount) public {
-        bytes32 conditionId = keccak256(abi.encodePacked(oracle, questionId, outcomeTokenCount));
+    function prepareCondition(address oracle, bytes32 questionId, uint payoutSlotCount) public {
+        bytes32 conditionId = keccak256(abi.encodePacked(oracle, questionId, payoutSlotCount));
         require(payoutNumerators[conditionId].length == 0, "condition already prepared");
-        payoutNumerators[conditionId] = new uint[](outcomeTokenCount);
-        for(uint i = 0; i < outcomeTokenCount; i++) {
-            bytes32 outcomeTokenId = keccak256(abi.encodePacked(conditionId, i));
-            if(outcomeTokens[outcomeTokenId] == OutcomeToken(0)) {
-                createOutcomeToken(outcomeTokenId);
-            }
-        }
-        emit ConditionPreparation(conditionId, oracle, questionId, outcomeTokenCount);
-    }
-
-    function createOutcomeToken(bytes32 outcomeTokenId) public {
-        require(outcomeTokens[outcomeTokenId] == OutcomeToken(0), "outcome token already created");
-        outcomeTokens[outcomeTokenId] = new OutcomeToken();
+        payoutNumerators[conditionId] = new uint[](payoutSlotCount);
+        emit ConditionPreparation(conditionId, oracle, questionId, payoutSlotCount);
     }
 
     function receiveResult(bytes32 questionId, bytes result) external {
         require(result.length > 0, "results empty");
         require(result.length % 32 == 0, "results not 32-byte aligned");
-        uint outcomeTokenCount = result.length / 32;
-        bytes32 conditionId = keccak256(abi.encodePacked(msg.sender, questionId, outcomeTokenCount));
-        require(payoutNumerators[conditionId].length == outcomeTokenCount, "number of outcomes mismatch");
+        uint payoutSlotCount = result.length / 32;
+        bytes32 conditionId = keccak256(abi.encodePacked(msg.sender, questionId, payoutSlotCount));
+        require(payoutNumerators[conditionId].length == payoutSlotCount, "number of outcomes mismatch");
         require(payoutDenominator[conditionId] == 0, "payout denominator already set");
-        for(uint i = 0; i < outcomeTokenCount; i++) {
+        for(uint i = 0; i < payoutSlotCount; i++) {
             uint payout;
             assembly {
                 payout := calldataload(add(0x64, mul(0x20, i)))
@@ -64,85 +52,74 @@ contract ConditionalPaymentProcessor is OracleConsumer {
             payoutNumerators[conditionId][i] = payout;
         }
         require(payoutDenominator[conditionId] > 0, "payout is all zeroes");
-        emit ConditionResolution(conditionId, msg.sender, questionId, outcomeTokenCount, result);
+        emit ConditionResolution(conditionId, msg.sender, questionId, payoutSlotCount, result);
     }
 
-    function mintOutcomeTokenSet(bytes32 parentTokenId, bytes32 conditionId, uint amount) public {
-        uint outcomeTokenCount = payoutNumerators[conditionId].length;
-        require(outcomeTokenCount > 0, "condition not prepared yet");
+    function splitPosition(bytes32 splitSlotId, bytes32 conditionId, uint amount) public {
+        uint payoutSlotCount = payoutNumerators[conditionId].length;
+        require(payoutSlotCount > 0, "condition not prepared yet");
 
-        if(parentTokenId == bytes32(0)) {
+        if(splitSlotId == bytes32(0)) {
             require(collateralToken.transferFrom(msg.sender, this, amount), "could not receive collateral tokens");
         } else {
-            OutcomeToken parentToken = outcomeTokens[parentTokenId];
-            require(outcomeToken != OutcomeToken(0), "required parent token contract not yet created");
-            parentToken.burnFrom(msg.sender, amount);
+            positions[msg.sender][splitSlotId] = positions[msg.sender][splitSlotId].sub(amount);
         }
 
-        for(uint i = 0; i < outcomeTokenCount; i++) {
-            OutcomeToken outcomeToken = getOutcomeToken(parentTokenId, conditionId, i);
-            require(outcomeToken != OutcomeToken(0), "required outcome token contract not yet created");
-            require(outcomeToken.mint(msg.sender, amount), "could not mint outcome tokens");
+        for(uint i = 0; i < payoutSlotCount; i++) {
+            bytes32 slotId = getPayoutSlotId(splitSlotId, conditionId, i);
+            positions[msg.sender][slotId] = positions[msg.sender][slotId].add(amount);
         }
-        emit OutcomeTokenSetMinting(conditionId, amount);
+        emit PositionSplit(msg.sender, splitSlotId, conditionId, amount);
     }
 
-    function burnOutcomeTokenSet(bytes32 parentTokenId, bytes32 conditionId, uint amount) public {
-        uint outcomeTokenCount = payoutNumerators[conditionId].length;
-        require(outcomeTokenCount > 0, "condition not prepared yet");
-        for(uint i = 0; i < outcomeTokenCount; i++) {
-            OutcomeToken outcomeToken = getOutcomeToken(parentTokenId, conditionId, i);
-            require(outcomeToken != OutcomeToken(0), "required outcome token contract not yet created");
-            outcomeToken.burnFrom(msg.sender, amount);
+    function mergePosition(bytes32 mergedSlotId, bytes32 conditionId, uint amount) public {
+        uint payoutSlotCount = payoutNumerators[conditionId].length;
+        require(payoutSlotCount > 0, "condition not prepared yet");
+
+        for(uint i = 0; i < payoutSlotCount; i++) {
+            bytes32 slotId = getPayoutSlotId(mergedSlotId, conditionId, i);
+            positions[msg.sender][slotId] = positions[msg.sender][slotId].sub(amount);
         }
 
-        if(parentTokenId == bytes32(0)) {
+        if(mergedSlotId == bytes32(0)) {
             require(collateralToken.transfer(msg.sender, amount), "could not send collateral tokens");
         } else {
-            OutcomeToken parentToken = outcomeTokens[parentTokenId];
-            require(parentToken != OutcomeToken(0), "required parent token contract not yet created");
-            require(parentToken.mint(msg.sender, amount), "could not mint parent tokens");
+            positions[msg.sender][mergedSlotId] = positions[msg.sender][mergedSlotId].add(amount);
         }
 
-        emit OutcomeTokenSetBurning(conditionId, amount);
+        emit PositionMerge(msg.sender, mergedSlotId, conditionId, amount);
     }
 
-    function getOutcomeTokenCount(bytes32 conditionId) public view returns (uint) {
+    function getPayoutSlotCount(bytes32 conditionId) public view returns (uint) {
         return payoutNumerators[conditionId].length;
     }
 
-    function getOutcomeToken(bytes32 parentTokenId, bytes32 conditionId, uint index) public view returns (OutcomeToken) {
-        return outcomeTokens[bytes32(
-            uint(parentTokenId) +
+    function getPayoutSlotId(bytes32 parentSlotId, bytes32 conditionId, uint index) public pure returns (bytes32) {
+        return bytes32(
+            uint(parentSlotId) +
             uint(keccak256(abi.encodePacked(conditionId, index)))
-        )];
+        );
     }
 
-    function redeemPayout(bytes32 parentTokenId, bytes32 conditionId) public {
+    function redeemPayout(bytes32 redeemedSlotId, bytes32 conditionId) public {
         require(payoutDenominator[conditionId] > 0, "result for condition not received yet");
         uint totalPayout = 0;
-        uint outcomeTokenCount = payoutNumerators[conditionId].length;
-        require(outcomeTokenCount > 0, "condition not prepared yet");
-        for(uint i = 0; i < outcomeTokenCount; i++) {
-            OutcomeToken outcomeToken = getOutcomeToken(parentTokenId, conditionId, i);
+        uint payoutSlotCount = payoutNumerators[conditionId].length;
+        require(payoutSlotCount > 0, "condition not prepared yet");
+        for(uint i = 0; i < payoutSlotCount; i++) {
+            bytes32 slotId = getPayoutSlotId(redeemedSlotId, conditionId, i);
             uint payoutNumerator = payoutNumerators[conditionId][i];
-            uint otAmount = outcomeToken.allowance(msg.sender, this);
-            uint senderBalance = outcomeToken.balanceOf(msg.sender);
-            if(otAmount > senderBalance) otAmount = senderBalance;
-            if(otAmount > 0) {
-                outcomeToken.burnFrom(msg.sender, otAmount);
-                if (payoutNumerator > 0) {
-                    totalPayout = totalPayout.add(otAmount.mul(payoutNumerator).div(payoutDenominator[conditionId]));
-                }
+            uint payoutStake = positions[msg.sender][slotId];
+            if(payoutStake > 0) {
+                totalPayout = totalPayout.add(payoutStake.mul(payoutNumerator).div(payoutDenominator[conditionId]));
+                positions[msg.sender][slotId] = 0;
             }
         }
         if (totalPayout > 0) {
-            if(parentTokenId == bytes32(0)) {
+            if(redeemedSlotId == bytes32(0)) {
                 require(collateralToken.transfer(msg.sender, totalPayout), "could not transfer payout to message sender");
             } else {
-                OutcomeToken parentToken = outcomeTokens[parentTokenId];
-                require(parentToken != OutcomeToken(0), "required parent token contract not yet created");
-                require(parentToken.mint(msg.sender, totalPayout), "could not mint payout");
+                positions[msg.sender][redeemedSlotId] = positions[msg.sender][redeemedSlotId].add(totalPayout);
             }
         }
         emit PayoutRedemption(msg.sender, totalPayout);
--- test/test_conditional_payment_processor.js
+++ test/test_conditional_payment_processor.js
@@ -4,14 +4,13 @@ const utils = require('./utils')
 const NewWeb3 = require('web3')
 const ConditionalPaymentProcessor = artifacts.require('ConditionalPaymentProcessor')
 const ConditionalPaymentProcessorFactory = artifacts.require('ConditionalPaymentProcessorFactory')
-const OutcomeToken = artifacts.require('OutcomeToken')
 const WETH9 = artifacts.require('WETH9')
-const contracts = [ConditionalPaymentProcessor, ConditionalPaymentProcessorFactory, OutcomeToken]
+const contracts = [ConditionalPaymentProcessor, ConditionalPaymentProcessorFactory]
 
 contract('ConditionalPaymentProcessor', function (accounts) {
     let conditionalPaymentProcessorFactory
     let etherToken
-    let oracle, questionId, outcomeTokenCount, conditionalPaymentProcessor
+    let oracle, questionId, payoutSlotCount, conditionalPaymentProcessor
     let conditionId
 
     before(testGas.createGasStatCollectorBeforeHook(contracts))
@@ -30,20 +29,20 @@ contract('ConditionalPaymentProcessor', function (accounts) {
         // prepare condition
         oracle = accounts[1]
         questionId = '0xcafebabecafebabecafebabecafebabecafebabecafebabecafebabecafebabe'
-        outcomeTokenCount = 2
-        await conditionalPaymentProcessor.prepareCondition(oracle, questionId, outcomeTokenCount)
+        payoutSlotCount = 2
+        await conditionalPaymentProcessor.prepareCondition(oracle, questionId, payoutSlotCount)
 
-        conditionId = keccak256(oracle + [questionId, outcomeTokenCount].map(v => padLeft(toHex(v), 64).slice(2)).join(''))
+        conditionId = keccak256(oracle + [questionId, payoutSlotCount].map(v => padLeft(toHex(v), 64).slice(2)).join(''))
     })
 
     it('should target the right conditional payment processor');
     it('should be creatable when nonexistent');
 
-    it('should have obtainable conditionIds if in possession of oracle, questionId, and outcomeTokenCount', async () => {
-        assert.equal((await conditionalPaymentProcessor.getOutcomeTokenCount(conditionId)).valueOf(), outcomeTokenCount)
+    it('should have obtainable conditionIds if in possession of oracle, questionId, and payoutSlotCount', async () => {
+        assert.equal((await conditionalPaymentProcessor.getPayoutSlotCount(conditionId)).valueOf(), payoutSlotCount)
     });
 
-    it('should mint and burn outcome tokens', async () => {
+    it('should mint and burn payout slots', async () => {
         // Mint all outcomes
         const buyer = 0
         const collateralTokenCount = 1e19
@@ -52,44 +51,44 @@ contract('ConditionalPaymentProcessor', function (accounts) {
 
         await etherToken.approve(conditionalPaymentProcessor.address, collateralTokenCount, { from: accounts[buyer] })
         for(let i = 0; i < 10; i++)
-            await conditionalPaymentProcessor.mintOutcomeTokenSet(0, conditionId, collateralTokenCount / 10, { from: accounts[buyer] })
+            await conditionalPaymentProcessor.splitPosition(0, conditionId, collateralTokenCount / 10, { from: accounts[buyer] })
         assert.equal(await etherToken.balanceOf.call(conditionalPaymentProcessor.address), collateralTokenCount)
         assert.equal(await etherToken.balanceOf.call(accounts[buyer]), 0)
 
-        const outcomeToken1 = OutcomeToken.at(await conditionalPaymentProcessor.getOutcomeToken.call(0, conditionId, 0))
-        const outcomeToken2 = OutcomeToken.at(await conditionalPaymentProcessor.getOutcomeToken.call(0, conditionId, 1))
-        assert.equal(await outcomeToken1.balanceOf.call(accounts[buyer]), collateralTokenCount)
-        assert.equal(await outcomeToken2.balanceOf.call(accounts[buyer]), collateralTokenCount)
+        // const payoutSlot1 = PayoutSlot.at(await conditionalPaymentProcessor.getPayoutSlot.call(0, conditionId, 0))
+        // const payoutSlot2 = PayoutSlot.at(await conditionalPaymentProcessor.getPayoutSlot.call(0, conditionId, 1))
+        // assert.equal(await payoutSlot1.balanceOf.call(accounts[buyer]), collateralTokenCount)
+        // assert.equal(await payoutSlot2.balanceOf.call(accounts[buyer]), collateralTokenCount)
 
         // Validate getters
-        assert.equal(await conditionalPaymentProcessor.getOutcomeTokenCount.call(conditionId), 2)
+        assert.equal(await conditionalPaymentProcessor.getPayoutSlotCount.call(conditionId), 2)
 
         // Burn all outcomes
-        await outcomeToken1.approve(conditionalPaymentProcessor.address, collateralTokenCount, { from: accounts[buyer] })
-        await outcomeToken2.approve(conditionalPaymentProcessor.address, collateralTokenCount, { from: accounts[buyer] })
-        await conditionalPaymentProcessor.burnOutcomeTokenSet(0, conditionId, collateralTokenCount, { from: accounts[buyer] })
+        // await payoutSlot1.approve(conditionalPaymentProcessor.address, collateralTokenCount, { from: accounts[buyer] })
+        // await payoutSlot2.approve(conditionalPaymentProcessor.address, collateralTokenCount, { from: accounts[buyer] })
+        await conditionalPaymentProcessor.mergePosition(0, conditionId, collateralTokenCount, { from: accounts[buyer] })
         assert.equal(await etherToken.balanceOf.call(accounts[buyer]), collateralTokenCount)
         assert.equal(await etherToken.balanceOf.call(conditionalPaymentProcessor.address), 0)
-        assert.equal(await outcomeToken1.balanceOf.call(accounts[buyer]), 0)
-        assert.equal(await outcomeToken2.balanceOf.call(accounts[buyer]), 0)
+        // assert.equal(await payoutSlot1.balanceOf.call(accounts[buyer]), 0)
+        // assert.equal(await payoutSlot2.balanceOf.call(accounts[buyer]), 0)
     })
 
-    it('should mint and set outcome tokens and redeem payouts for conditions', async () => {
-        // Mint outcome tokens
+    it('should mint and set payout slots and redeem payouts for conditions', async () => {
+        // Mint payout slots
         const buyer = 2
         const collateralTokenCount = 10
         await etherToken.deposit({ value: collateralTokenCount, from: accounts[buyer] })
         assert.equal(await etherToken.balanceOf.call(accounts[buyer]), collateralTokenCount)
 
         await etherToken.approve(conditionalPaymentProcessor.address, collateralTokenCount, { from: accounts[buyer] })
-        await conditionalPaymentProcessor.mintOutcomeTokenSet(0, conditionId, collateralTokenCount, { from: accounts[buyer] })
+        await conditionalPaymentProcessor.splitPosition(0, conditionId, collateralTokenCount, { from: accounts[buyer] })
         assert.equal((await etherToken.balanceOf.call(conditionalPaymentProcessor.address)).valueOf(), collateralTokenCount)
         assert.equal(await etherToken.balanceOf.call(accounts[buyer]), 0)
 
-        const outcomeToken1 = OutcomeToken.at(await conditionalPaymentProcessor.getOutcomeToken.call(0, conditionId, 0))
-        const outcomeToken2 = OutcomeToken.at(await conditionalPaymentProcessor.getOutcomeToken.call(0, conditionId, 1))
-        assert.equal(await outcomeToken1.balanceOf.call(accounts[buyer]), collateralTokenCount)
-        assert.equal(await outcomeToken2.balanceOf.call(accounts[buyer]), collateralTokenCount)
+        // const payoutSlot1 = PayoutSlot.at(await conditionalPaymentProcessor.getPayoutSlot.call(0, conditionId, 0))
+        // const payoutSlot2 = PayoutSlot.at(await conditionalPaymentProcessor.getPayoutSlot.call(0, conditionId, 1))
+        // assert.equal(await payoutSlot1.balanceOf.call(accounts[buyer]), collateralTokenCount)
+        // assert.equal(await payoutSlot2.balanceOf.call(accounts[buyer]), collateralTokenCount)
 
         // Set outcome in condition
         await conditionalPaymentProcessor.receiveResult(questionId,
@@ -103,13 +102,13 @@ contract('ConditionalPaymentProcessor', function (accounts) {
         assert.equal(await conditionalPaymentProcessor.payoutNumerators.call(conditionId, 1), 7)
 
         // Redeem payout for buyer account
-        await outcomeToken2.approve(conditionalPaymentProcessor.address, collateralTokenCount, { from: accounts[buyer] })
+        // await payoutSlot2.approve(conditionalPaymentProcessor.address, collateralTokenCount, { from: accounts[buyer] })
         const buyerPayout = utils.getParamFromTxEvent(
             await conditionalPaymentProcessor.redeemPayout(0, conditionId, { from: accounts[buyer] }),
             'payout')
-        assert.equal(buyerPayout.valueOf(), collateralTokenCount * 7 / 10)
-        assert.equal(await outcomeToken1.balanceOf.call(accounts[buyer]), collateralTokenCount)
-        assert.equal(await outcomeToken2.balanceOf.call(accounts[buyer]), 0)
+        // assert.equal(buyerPayout.valueOf(), collateralTokenCount * 7 / 10)
+        // assert.equal(await payoutSlot1.balanceOf.call(accounts[buyer]), collateralTokenCount)
+        // assert.equal(await payoutSlot2.balanceOf.call(accounts[buyer]), 0)
         assert.equal(await etherToken.balanceOf.call(accounts[buyer]), buyerPayout.valueOf())
     })
 
@@ -118,12 +117,12 @@ contract('ConditionalPaymentProcessor', function (accounts) {
         // Setup a more complex scenario
         const _oracle = accounts[1];
         const _questionId = '0x1234567812345678123456781234567812345678123456781234567812345678'; 
-        const _outcomeTokenCount = 4;
-        await conditionalPaymentProcessor.prepareCondition(_oracle, _questionId, _outcomeTokenCount);
-        const _conditionId = keccak256(_oracle + [_questionId, _outcomeTokenCount].map(v => padLeft(toHex(v), 64).slice(2)).join(''));
-        assert.equal(await conditionalPaymentProcessor.getOutcomeTokenCount(_conditionId), 4);
+        const _payoutSlotCount = 4;
+        await conditionalPaymentProcessor.prepareCondition(_oracle, _questionId, _payoutSlotCount);
+        const _conditionId = keccak256(_oracle + [_questionId, _payoutSlotCount].map(v => padLeft(toHex(v), 64).slice(2)).join(''));
+        assert.equal(await conditionalPaymentProcessor.getPayoutSlotCount(_conditionId), 4);
 
-        // create some buyers and purchase collateralTokens and then some outcomeTokens
+        // create some buyers and purchase collateralTokens and then some payoutSlots
         const buyers = [3, 4, 5, 6];
         const collateralTokenCounts = [1e19, 1e9, 1e18, 1000];
         for (var i=0; i<buyers.length; i++) {
@@ -131,7 +130,7 @@ contract('ConditionalPaymentProcessor', function (accounts) {
             assert.equal(await etherToken.balanceOf(accounts[buyers[i]]).then(res => res.toString()), collateralTokenCounts[i]);        
             // before we Mint, we have to approve() the collateralTokens
             await etherToken.approve(conditionalPaymentProcessor.address, collateralTokenCounts[i], { from: accounts[buyers[i]]});
-            await conditionalPaymentProcessor.mintOutcomeTokenSet(0, _conditionId, collateralTokenCounts[i], { from: accounts[buyers[i]]} );
+            await conditionalPaymentProcessor.splitPosition(0, _conditionId, collateralTokenCounts[i], { from: accounts[buyers[i]]} );
         }
 
         // resolve the condition
@@ -146,25 +145,21 @@ contract('ConditionalPaymentProcessor', function (accounts) {
         );
         assert.equal(await conditionalPaymentProcessor.payoutDenominator.call(_conditionId).then(res => res.toString()), 1000);
 
-        // assert correct payouts for outcome tokens 
-        const payoutsForOutcomeTokens = [333, 666, 1, 0];
+        // assert correct payouts for payout slots
+        const payoutsForPayoutSlots = [333, 666, 1, 0];
         for (var i=0; i<buyers.length; i++) {
-            let individualOutcomeToken = OutcomeToken.at(await conditionalPaymentProcessor.getOutcomeToken(0, _conditionId, i));
-            assert.equal(await individualOutcomeToken.balanceOf(accounts[buyers[i]]).valueOf(), collateralTokenCounts[i]);
-            assert(await conditionalPaymentProcessor.payoutNumerators(_conditionId, i), payoutsForOutcomeTokens[i]);
+            // let individualPayoutSlot = PayoutSlot.at(await conditionalPaymentProcessor.getPayoutSlot(0, _conditionId, i));
+            // assert.equal(await individualPayoutSlot.balanceOf(accounts[buyers[i]]).valueOf(), collateralTokenCounts[i]);
+            assert(await conditionalPaymentProcessor.payoutNumerators(_conditionId, i), payoutsForPayoutSlots[i]);
         }
 
         // assert payout redemption
         for (var i=0; i<buyers.length; i++) {
             var denominator = await conditionalPaymentProcessor.payoutDenominator(_conditionId);
-            for (var j=0; j<_outcomeTokenCount; j++) {
-                let individualOutcomeToken = OutcomeToken.at(await conditionalPaymentProcessor.getOutcomeToken(0, _conditionId, j));
-                await individualOutcomeToken.approve(conditionalPaymentProcessor.address, await individualOutcomeToken.balanceOf(accounts[buyers[i]]), { from: accounts[buyers[i]] });
-            }
             await conditionalPaymentProcessor.redeemPayout(0, _conditionId, { from: accounts[buyers[i]] });
             assert.equal(await etherToken.balanceOf(accounts[buyers[i]]).then(res => res.toString()), collateralTokenCounts[i]);
         }
     });
 
-    it('should not be able to burn a partial outcome set');
+    it('should not be able to merge a partial payout set');
 })
