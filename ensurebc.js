51fcfd9a11198d6d2c40806f7d1f93ab7de0c232
Alan Lu <alanlu1023@gmail.com>
2018-10-22 18:47:17 -0500

    Backwards compatibility and packaging niceness

---
+++ ensurebc.js
@@ -0,0 +1,18 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+const artifactsDir = path.join('build', 'contracts')
+
+for(const [newName, bcName] of [
+    ['ERC20', 'Token'],
+    ['DetailedERC20', 'HumanFriendlyToken'],
+    ['WETH9', 'EtherToken'],
+    ['Fixed192x64Math', 'Math'],
+])
+    fs.copySync(path.join(artifactsDir, newName + '.json'), path.join(artifactsDir, bcName + '.json'))
+
+const gasStatsFile = path.join('build', 'gas-stats.json')
+const gasStatsObj = fs.readJsonSync(gasStatsFile)
+gasStatsObj.EtherToken = gasStatsObj.WETH9
+gasStatsObj.Token = gasStatsObj.ERC20
+fs.writeJsonSync(gasStatsFile, gasStatsObj, { spaces: 2 })
